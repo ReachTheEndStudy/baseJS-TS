b20b39e68e87f65b4f1c0bc62acf10c4f46ea642
ReachTheEnd <reachtheendstudy@gmail.com>
2024-11-25 09:58:52 +0300

    add lesson 14

---
+++ 14/hw.js
@@ -0,0 +1,26 @@
+/*
+
+Теория:
+1. https://doka.guide/js/events/
+2. https://doka.guide/js/element-addeventlistener/
+3. https://learn.javascript.ru/searching-elements-dom
+4. https://learn.javascript.ru/introduction-browser-events
+5. https://learn.javascript.ru/bubbling-and-capturing
+6. https://learn.javascript.ru/mouse-events-basics
+7. https://learn.javascript.ru/mousemove-mouseover-mouseout-mouseenter-mouseleave
+8. https://learn.javascript.ru/events-change-input
+
+
+Практика:
+
+Решить задачи с codewars
+1. https://www.codewars.com/kata/55908aad6620c066bc00002a/train/javascript
+2. https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/javascript
+3. https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/javascript
+
+Накодить:
+В html документе добавить поле ввода и поле со значением 100,
+используем <input type='text' /> и <p>. Нужно что бы, введенное значение из
+input отображалось в поле с тегом <p>. Для контроля ввода input используем
+ивент 'input'
+*/
---
+++ 14/lesson.js
@@ -0,0 +1,32 @@
+const p = document.getElementById('p')
+const button = document.getElementById('button')
+const input = document.getElementById('input')
+
+
+// console.log(p)
+// console.log(button)
+
+
+// console.log(p.innerText)
+// console.log(button.innerText)
+
+
+// button.addEventListener('click', (event) => {
+//     console.log(event)
+// })
+
+
+
+// button.addEventListener('mouseover', (event) => {
+//     console.log(event)
+// })
+
+
+// button.addEventListener('mouseleave', (event) => {
+//     console.log(event)
+// })
+
+
+// button.addEventListener('click', () => {
+//     p.innerText = p.innerText + 1
+// })
